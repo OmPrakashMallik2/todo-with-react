f48485b64ba636f99c974f691a8d4746c883faff
Om Prakash Mallik <opm202000@gmail.com>
2024-02-03 11:42:42 +0530

    Creating to do llist with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>TO DO List</h1>
+
+      <div className='input-filed'>
+        <input type='text' placeholder='Enter your task' />
+        <button>Add</button>
+      </div>
+
+      <div className='tasks'>
+        <h2>Task here</h2>
+      </div>
+
     </div>
   );
 }
